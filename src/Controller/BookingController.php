b2befbd58fcaf7f06f368c7d821d9515b7b369ff
Arthur <ath.engels@gmail.com>
2019-11-03 14:03:01 +0100

    réglage controller

--- src/Controller/BookingController.php
+++ src/Controller/BookingController.php
@@ -3,12 +3,10 @@
 
 namespace App\Controller;
 
-
 class BookingController extends AbstractController
 {
     public function booking()
     {
         return $this->twig->render('Booking/booking.html.twig');
     }
-
 }
